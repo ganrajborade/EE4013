cbce68d833e539e3e4a72698e9ff87ebfbb95dd1
Ganraj A. Borade <47532013+ganrajborade@users.noreply.github.com>
2023-05-08 19:55:26 +0530

    Create rename.py

---
+++ voice recognition/rename.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+
+os.chdir("/home/ganra/Desktop/Voice Recognition/Samples/Stop")
+
+path = os.getcwd()
+print path
+filenames = os.listdir(path)
+i=0
+for f in filenames:
+	a = "stop"+str(i)+".wav"
+	os.rename(f,a)
+	i = i+1
+	print a
+			
